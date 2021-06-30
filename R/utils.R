67767d4da6f70b2f14b3ea2de3d8fc0e73fab3f7
Michael Sumner <mdsumner@gmail.com>
2021-06-30 19:51:14 +1000

    update

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,7 @@
 #'
 #' @return vector of hex codes
 #' @export
-#'
+#' @importFrom grDevices grey rgb
 rayshade_to_hex <- function(x) {
   if (length(dim(x)) == 3 ) {
     rgb(t(x[,,1]), t(x[,,2]), t(x[,,3]))
